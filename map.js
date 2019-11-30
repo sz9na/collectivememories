bf821235401b531d9202e4df3ed980612dc116eb
sz9na <56407114+sz9na@users.noreply.github.com>
2019-11-30 02:20:53 -0500

    1130
    Update

--- map.js
+++ map.js
@@ -133,6 +133,32 @@ function isElementOnScreen(id) {
         });
     });
 
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 // Reset map button
     $("#reset").click(function() 
     {
@@ -178,3 +204,15 @@ function isElementOnScreen(id) {
       // Add the popup to the map 
       popup.addTo(map);  // replace "map" with the name of the variable in line 4, if different
   });
+
+//sidebar
+
+function openNav() {
+  document.getElementById("sidebar").style.width = "400px";
+  document.getElementById("main").style.marginLeft = "400px";
+}
+
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
